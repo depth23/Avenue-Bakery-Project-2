66e8ebefa7af3957384024a7eb451addb770a000
depth23 <deon.p.sharps@gmail.com>
2015-11-18 22:34:44 -0700

    menu

--- public/js/avBCtrl.js
+++ public/js/avBCtrl.js
@@ -2,7 +2,22 @@ var app = angular.module('avBApp');
 
 app.controller('avBCtrl', function($scope, avBSrvc, $location) {
 
-    $scope.logOutUser = function() {
+//  Checked for currently logged in
+//  avBSrvc.getUserName().then(function(res) {
+//      if (res.data.admin === true) {
+//        $scope.customerName = Admin;
+//        $scope.user = res.data;
+//      }
+//      else {
+//        $scope.customerName = 'Welcome, ' + res.data.first;
+//        $scope.user = res.data;
+//        console.log('Current User', res);
+//      }
+//    });  
+  
+  
+  
+  $scope.logOutUser = function() {
         avBSrvc.logOutUser().then(function(response) {
             console.log(response);
             $location.path('/');
--- public/js/avBSrvc.js
+++ public/js/avBSrvc.js
@@ -2,12 +2,20 @@ var app = angular.module("avBApp");
 
 app.service("avBSrvc", function($http) {
 
-    this.logOutUser = function() {
-        return $http({
-            method: "GET",
-            url: "/logout"
+  
+//  this.getUserName = function() {
+//    return $http({
+//      method: "GET",
+//      url: "" 
+//    })
+//  };
+  
+  this.logOutUser = function() {
+    return $http({
+      method: "GET",
+      url: "/logout"
             
-        })
-    }
+    })
+  };
     
 });
