e8d5acf29e615f5765a3997c3b08e9131e640980
depth23 <deon.p.sharps@gmail.com>
2015-11-19 08:42:22 -0700

    styling

--- public/js/avBCtrl.js
+++ public/js/avBCtrl.js
@@ -3,17 +3,17 @@ var app = angular.module('avBApp');
 app.controller('avBCtrl', function($scope, avBSrvc, $location) {
 
 //  Checked for currently logged in
-  //avBSrvc.getUserName().then(function(res) {
+  avBSrvc.getUserName().then(function(res) {
 //      if (res.data.admin === true) {
 //        $scope.customerName = Admin;
 //        $scope.user = res.data;
 //      }
 //      else {
-  //     $scope.customerName = 'Welcome, ' + res.data.first;
+       $scope.customerName = 'Welcome, ' + res.data.first;
 //        $scope.user = res.data;
 //        console.log('Current User', res);
 //      }
-  //  });  
+    });  
   
   
   
