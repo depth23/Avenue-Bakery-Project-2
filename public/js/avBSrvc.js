31921fed27e4c53268494d04b8aa3a9a8bc93762
depth23 <deon.p.sharps@gmail.com>
2015-11-19 08:48:31 -0700

    styling

--- public/js/avBSrvc.js
+++ public/js/avBSrvc.js
@@ -3,14 +3,14 @@ var app = angular.module("avBApp");
 app.service("avBSrvc", function($http) {
 
   
-//  this.getUserName = function() {
-//    return $http({
-//      method: "GET",
-//      url: "/user" 
-//    }).then(function(res) {
-//      return res.data;
-//    })
-//  };
+  this.getUserName = function() {
+    return $http({
+      method: "GET",
+      url: "/user" 
+    }).then(function(res) {
+      return res.data;
+    })
+  };
   
   this.logOutUser = function() {
     return $http({
